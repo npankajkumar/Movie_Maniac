869cb08f43af8cacd16c72ab1bac3f3f2dae59c5
npankajkumar <npankajkumar@actalentservices.com>
2024-09-26 00:32:08 +0530

    open api added

--- MovieManiac/src/components/MovieList/MovieCard.jsx
+++ MovieManiac/src/components/MovieList/MovieCard.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import "./MovieCard.css";
 import star from "../../assets/glowingstar.png";
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
   return (
     <a href="" className="movie_card">
       {" "}
       <img
-        src="https://i.etsystatic.com/18242346/r/il/fd61f8/2933715225/il_570xN.2933715225_a913.jpg"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         className="movie_poster"
         alt=""
       />
       <div className="movie_details">
-        <h3 className="movie_details_heading">Movie Name</h3>
+        <h3 className="movie_details_heading">{movie.original_title}</h3>
         <div className="movie_date_rate">
-          <p>10-10-2020</p>
+          <p>{movie.release_date}</p>
           <p>
-            8.2{" "} <img className="card_emoji" src={star} alt="" />
+            {movie.rate_average}{" "} <img className="card_emoji" src={star} alt="" />
           </p>
         </div>
         <p className="movie_description">
+          {movie.overview}
           {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          officiis aut mollitia reprehenderit ducimus{" "}
         </p>
       </div>
     </a>
--- MovieManiac/src/components/MovieList/MovieList.jsx
+++ MovieManiac/src/components/MovieList/MovieList.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './MovieList.css'
 import fire from '../../assets/fire.png'
 import MovieCard from './MovieCard'
 const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // Fetch movie data when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/movie/popular',
+          {
+            headers: {
+              accept: 'application/json',
+              Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMGJiMGU3NTMxNzhhNjk0ODY0Y2IwZGYwNWEzZWZlNCIsIm5iZiI6MTcyNzI4ODI3My4zNzUwNTMsInN1YiI6IjY2ZjQ1MDQ4M2E5NWE1YmNkYTIzMTMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E-cOEzWmFFge994kQnRU9tGFZ178J3ir5ji-3qnytzA',
+            },
+          }
+        );
+        console.log(response.data.results)
+        setMovies(response.data.results); // Assuming the relevant data is in the response
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className='movie_list'>
         <header  className='movie_list_header'>
@@ -32,7 +59,11 @@ const MovieList = () => {
         </header>
 
         <div className="movie_cards">
-            <MovieCard/>
+            {
+                movies.map(movie=>
+                    <MovieCard key={movie.id} movie={movie}/>
+                )
+            }
         </div>
 
     </section>
